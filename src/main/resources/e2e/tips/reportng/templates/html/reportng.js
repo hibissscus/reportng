d3d39adebf734807cf9770b7a2956c0dc17c851e
Sergei Stepanov <sergei.stepanov@ysura.com>
2020-09-29 20:27:18 +0200

    Changes in version 1.2.6 ------------------------ * Html inline images for tests are now clickable

--- src/main/resources/e2e/tips/reportng/templates/html/reportng.js
+++ src/main/resources/e2e/tips/reportng/templates/html/reportng.js
@@ -1,14 +1,26 @@
-function toggleElement(elementId, displayStyle)
-{
-    var element = document.getElementById(elementId);
-    var current = element.currentStyle
-                ? element.currentStyle['display']
-                : document.defaultView.getComputedStyle(element, null).getPropertyValue('display');
+function toggleElement(elementId, displayStyle) {
+    let element = document.getElementById(elementId);
+    let current = element.currentStyle
+        ? element.currentStyle['display']
+        : document.defaultView.getComputedStyle(element, null).getPropertyValue('display');
     element.style.display = (current == 'none' ? displayStyle : 'none');
 }
 
-function toggle(toggleId)
-{
-    var toggle = document.getElementById ? document.getElementById(toggleId) : document.all[toggleId];
+function toggle(toggleId) {
+    let toggle = document.getElementById ? document.getElementById(toggleId) : document.all[toggleId];
     toggle.textContent = toggle.innerHTML == '\u25b6' ? '\u25bc' : '\u25b6';
+}
+
+function modalClose() {
+    let modal = document.getElementById("modal");
+    modal.style.display = "none";
+}
+
+function modalImage(screenshotId) {
+    let screenshot = document.getElementById("screenshot-" + screenshotId);
+    let modal = document.getElementById("modal");
+    let modalImg = document.getElementById("modalImage");
+
+    modalImg.src = screenshot.src;
+    modal.style.display = "block";
 }

57ed3e6ec1e3cd51c9ea4b5c81bc7a62b9760ddf
sstepanov <hibissscus@gmail.com>
2023-01-03 15:48:55 +0100

    Changes in version 1.5.3 ------------------------ * TestNG 7.7.1 * Kotlin 1.8.0

--- src/main/kotlin/testee/it/reportng/ReportNGUtils.kt
+++ src/main/kotlin/testee/it/reportng/ReportNGUtils.kt
@@ -85,11 +85,11 @@ class ReportNGUtils {
      * Return first group to which test belongs.
      */
     fun getIncludedGroups(context: ITestContext): String {
-        return with(context.allTestMethods.mapNotNull { it.groups }.sortedByDescending { it.count() }) {
-            first().mapIndexed { index, e ->
-                listOfNotNull(e, last().getOrNull(index))
-            }
-        }.flatten().distinct().sorted().joinToString()
+        return context.allTestMethods
+            .mapNotNull { it.groups }
+            .sortedByDescending { it.count() }
+            .mapNotNull { it.firstOrNull()}
+            .toSet().distinct().sorted().joinToString()
     }
 
     /**
